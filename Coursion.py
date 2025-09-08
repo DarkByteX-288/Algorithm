8ef63371ea30cf54e41449e60ace6adee7dd0467
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2025-09-08 06:18:29 -0700

    Update Coursion.py

--- Coursion.py
+++ Coursion.py
@@ -1,7 +1,12 @@
+
 def factorial(n):
+"""factorial finder function 
+    we use this to implement recursion logic """
+
     if n == 0:
         return 1
     else:
         return n*factorial(n-1)
-    
+
+#test
 print(factorial(3))
