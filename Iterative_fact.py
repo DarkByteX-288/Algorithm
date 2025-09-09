15d8a31fd4ecd32c4c49f7c5e8d3e5ecb1c1dcf2
Omran Ahmadzai <ahmadzai.omran12@gmail.com>
2025-09-09 06:45:32 -0700

    Update Iterative_fact.py

--- Iterative_fact.py
+++ Iterative_fact.py
@@ -1,16 +1,15 @@
-#this program calculates factorial of a number using iterative approach
-
 def factorial(n):
-
+    '''This function is used to print factorial off a number 
+    main resaon of this is to implement iterative logic for factorial '''
+    
     if n == 0:
         return 1
-    
     fact = 1
     for i in range(1, n+1):
         fact *= i
     return fact
 
-
+#test
 print(factorial(5))  # Output: 120
 print(factorial(0))  # Output: 1
 
